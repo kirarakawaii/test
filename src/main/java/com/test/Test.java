4b86c7de8e7687a46e0061f93e4471f351fd440d
lirui <kirarakawaii@163.com>
2023-12-26 01:47:30 +0800

    master 演示冲突

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
         System.out.println("\"你好\" = " + "你好");
         System.out.println("\"演示冲突\" = " + "演示冲突");
         System.out.println("\"hi\" = " + "hi");
+        System.out.println("\"测试1\" = " + "测试1");
     }
 
 
